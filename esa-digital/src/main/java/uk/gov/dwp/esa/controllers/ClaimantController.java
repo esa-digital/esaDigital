cf000747cf199e00dc010c65e9db488fcf744c0f
Abilash Madhavan <abilash.madhavan@gmail.com>
2016-06-21 11:32:01 +0100

    changed the flow from personal details page to contact details page.

--- esa-digital/src/main/java/uk/gov/dwp/esa/controllers/ClaimantController.java
+++ esa-digital/src/main/java/uk/gov/dwp/esa/controllers/ClaimantController.java
@@ -22,7 +22,7 @@ public class ClaimantController {
 
 	private static final Logger logger = LogManager.getLogger(ClaimantController.class);
 	protected static final String CLAIMANT_DETAILS = "Claimant";
-	private String NEXT_FORM = "/api" + ControllerUrls.GP_DETAILS_FORM;
+	private String NEXT_FORM = "/api" + ControllerUrls.CONTACT_DETAILS;
 	private static String PERSONAL_DETAILS_FORM = "personal-details";
 	
 		
@@ -32,7 +32,8 @@ public class ClaimantController {
 	@Autowired
 	private GenericModelParser generator;
 	
-	private static final String PROPERTY_FILE="C:\\Users\\e.ratnakar.shetty\\git\\esaDigital\\esa-digital\\src\\main\\resources\\messages.properties";
+	//private static final String PROPERTY_FILE="C:\\Users\\e.ratnakar.shetty\\git\\esaDigital\\esa-digital\\src\\main\\resources\\messages.properties";
+	private static final String PROPERTY_FILE="C:\\GitRepos\\esaDigital\\esa-digital\\src\\main\\resources\\messages.properties";
 	
 		
 	@RequestMapping(value = ControllerUrls.PERSONAL_DETAILS_FORM, method = RequestMethod.GET)
